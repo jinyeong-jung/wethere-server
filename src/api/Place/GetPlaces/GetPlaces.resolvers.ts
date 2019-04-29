f1c7be0d1e9a1be1c8548b732ec93822a19bfd56
jinyeong-jung <jinyeong32@gmail.com>
2019-04-29 20:11:34 +0900

    Classify Places into visited ones and not visied ones

--- src/api/Place/GetPlaces/GetPlaces.resolvers.ts
+++ src/api/Place/GetPlaces/GetPlaces.resolvers.ts
@@ -3,6 +3,7 @@ import privateResolver from "../../../utils/privateResolver";
 import { GetPlacesResponse } from "../../../types/graph";
 import User from "../../../entities/User";
 import Couple from "../../../entities/Couple";
+import Place from "../../../entities/Place";
 
 const resolvers: Resolvers = {
   Query: {
@@ -19,24 +20,37 @@ const resolvers: Resolvers = {
             { relations: ["places"] }
           );
           const couple = coupleOne || coupleTwo;
+
+          const visitedPlaces = await Place.find({
+            couple,
+            isVisited: true
+          });
+          const notVisitedPlaces = await Place.find({
+            couple,
+            isVisited: false
+          });
+
           if (couple) {
             return {
               ok: true,
               error: null,
-              places: couple.places
+              visitedPlaces,
+              notVisitedPlaces
             };
           } else {
             return {
               ok: false,
               error: "저장된 장소가 없습니다",
-              places: null
+              visitedPlaces: null,
+              notVisitedPlaces: null
             };
           }
         } catch (error) {
           return {
             ok: false,
             error: error.message,
-            places: null
+            visitedPlaces: null,
+            notVisitedPlaces: null
           };
         }
       }
