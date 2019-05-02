7d5e257fc367b3260fa68d148689d14206ad4c6e
jinyeong-jung <jinyeong32@gmail.com>
2019-05-02 15:19:15 +0900

    Bugfix

--- src/app.ts
+++ src/app.ts
@@ -26,9 +26,9 @@ class App {
     this.middlewares();
   }
   private middlewares = (): void => {
-    this.app.express.use(cors());
-    this.app.express.use(logger("dev"));
     this.app.express.use(helmet());
+    this.app.express.use(logger("dev"));
+    this.app.express.use(cors());
     this.app.express.use(this.jwt);
   };
 
